fa7312dbab5abb71f0a0fd33dec50ceca25d743d
Ethan Bevan <74314722+ancientpi3@users.noreply.github.com>
2022-12-06 14:49:03 -0500

    Update basic_gan.py
    Fixed another bug with train_discriminator

--- modules/basic_gan.py
+++ modules/basic_gan.py
@@ -83,6 +83,6 @@ def define_generator(latent_dim):
 	model.add(Conv2D(1, (7,7), activation='sigmoid', padding='same'))
 	return model
 def train_discriminator(d_model, epochs=10):
-	labels,data = generate_data()
+	labels,data = generate_mixed_data()
 	valid_labels, valid_data = generate_mixed_data(mean=.2,scale=.3,data_size=50)
 	return d_model.fit(x = data, y = labels, epochs=epochs, validation_data=(valid_data,valid_labels))
