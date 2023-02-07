2e771184cb4d1b8db8169f752369493df3a3af60
Ethan Bevan <74314722+ancientpi3@users.noreply.github.com>
2023-02-07 12:16:10 -0500

    Update DCGAN.py
    Redesigned GAN using functional API. This code will act as the API for the NASA Fellowship study.

--- modules/DCGAN.py
+++ modules/DCGAN.py
@@ -5,75 +5,60 @@ import cv2
 import numpy as np
 import cv2
 import os
-
-
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Reshape
-from keras.layers import Flatten
-from keras.layers import Conv2D
-from keras.layers import Conv2DTranspose
-from keras.layers import LeakyReLU
-from keras.layers import ReLU
-from keras.layers import BatchNormalization
+from PIL import Image
 from keras.initializers import RandomNormal
-from keras.optimizers import RMSprop
+
 
 
 def discriminator(in_shape=(64,64,3),dim=64):
-
   init = RandomNormal(stddev=0.02)
-  model = Sequential()
+  discriminator_input = keras.Input(shape=(64,64,3),name="d_input")
+  
+  x = keras.layers.Conv2D(64,(5,5),strides = (2,2),padding = "same",kernel_initializer=init, activation="leaky_relu")(discriminator_input)
+  x = keras.layers.BatchNormalization(momentum=.9)(x,training=False)
 
-  model.add(Conv2D(dim, (5,5), strides=(2,2), padding='same', kernel_initializer=init, input_shape=in_shape))
-  model.add(BatchNormalization())
-  model.add(LeakyReLU())
+  x = keras.layers.Conv2D(128,(5,5),strides = (2,2),padding = "same",kernel_initializer=init, activation="leaky_relu")(x)
+  x = keras.layers.BatchNormalization(momentum=.9)(x,training=False)
 
-  model.add(Conv2D(2*dim, (5,5), strides=(2,2), padding='same', kernel_initializer=init))
-  model.add(BatchNormalization())
-  model.add(LeakyReLU())
+  x = keras.layers.Conv2D(256,(5,5),strides = (2,2),padding = "same",kernel_initializer=init, activation="leaky_relu")(x)
+  x = keras.layers.BatchNormalization(momentum=.9)(x,training=False)
 
-  model.add(Conv2D(4*dim, (5,5), strides=(2,2), padding='same', kernel_initializer=init))
-  model.add(BatchNormalization())
-  model.add(LeakyReLU())
+  x = keras.layers.Conv2D(512,(5,5),strides = (2,2),padding = "same",kernel_initializer=init, activation="leaky_relu")(x)
+  x = keras.layers.BatchNormalization(momentum=.9)(x,training=False)
+  x = keras.layers.Flatten()(x)
+  
+  discriminator_output = keras.layers.Dense(1, activation='sigmoid')(x)
 
-  model.add(Conv2D(8*dim, (5,5), strides=(2,2), padding='same', kernel_initializer=init))
-  model.add(BatchNormalization())
-  model.add(LeakyReLU())
 
-  model.add(Flatten())
-  model.add(Dense(1))
-
+  model = keras.Model(discriminator_input, discriminator_output,name="discriminator")
   opt = keras.optimizers.Adam(lr=0.0002, beta_1=0.5)
-  model.compile(loss="binary_crossentropy", optimizer=opt)
+
+  model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
   return model
 
 
 def generator(latent_dim=128,dim=64):
 
-  init = RandomNormal(stddev=0.02)
-  model = Sequential()
+  generator_input = keras.Input(shape=(latent_dim),name="g_input")
+  n_nodes = 4*4*8*64
 
-  n_nodes = 4*4*8*dim
-  model.add(Dense(n_nodes, kernel_initializer=init, input_dim=latent_dim))
-  model.add(Reshape((4, 4, 8*dim)))
-  model.add(BatchNormalization())
-  model.add(ReLU())
+  x = keras.layers.Dense(n_nodes)(generator_input)
+  x = keras.layers.Reshape((4,4,8*64))(x)
+  x = keras.layers.BatchNormalization(momentum=.9)(x,training=True)
 
-  model.add(Conv2DTranspose(4*dim, (5,5), strides=(2,2), padding='same', kernel_initializer=init))
-  model.add(BatchNormalization())
-  model.add(ReLU())
+  x = keras.layers.Conv2DTranspose(256,(5,5),strides=(2,2),padding='same',activation="relu")(x)
+  x = keras.layers.BatchNormalization(momentum=.9)(x,training=True)
 
-  model.add(Conv2DTranspose(2*dim, (5,5), strides=(2,2), padding='same', kernel_initializer=init))
-  model.add(BatchNormalization())
-  model.add(ReLU())
+  x = keras.layers.Conv2DTranspose(128,(5,5),strides=(2,2),padding='same',activation="relu")(x)
+  x = keras.layers.BatchNormalization(momentum=.9)(x,training=True)
 
-  model.add(Conv2DTranspose(dim, (5,5), strides=(2,2), padding='same', kernel_initializer=init))
-  model.add(BatchNormalization())
-  model.add(ReLU())
+  x = keras.layers.Conv2DTranspose(64,(5,5),strides=(2,2),padding='same',activation="relu")(x)
+  x = keras.layers.BatchNormalization(momentum=.9)(x,training=True)
 
-  model.add(Conv2DTranspose(3, (5,5), strides=(2,2),activation='tanh', padding='same', kernel_initializer=init))
-  return model
+  generator_output = keras.layers.Conv2DTranspose(3,(5,5),strides=(2,2),padding='same',activation="tanh")(x)
+
+
+  return keras.Model(generator_input, generator_output,name="generator")
 
 def GAN(d_model,g_model):
 	d_model.trainable = False
@@ -100,32 +85,36 @@ def save_plot(examples, epoch, n=5):
 	for i in range(25):
 		plt.subplot(n, n, 1 + i)
 		plt.axis('off')
-		plt.imshow(examples[i, :, :, 0], cmap='gray')
+		plt.imshow((examples[i, :, :]+1)/2)
 	filename = 'generated_plot_e%03d.png' % (epoch+1)
 	plt.show()
 	#plt.savefig(filename)
 	plt.close()
  
+def train(d_model,g_model,gan_model, x_train, epochs=400, batches_per_epoch=64, verbose=1, print_every=20):
+  latent_dim = g_model.input.shape[1]
+  batch_size = 24
+  half_batch = int(batch_size/2)
+  batches_per_epoch = 64
+  save_plot(grab_fake_batch(g_model,50).reshape(50,64,64,3),epoch=0)
+  for epoch in range(epochs):
+    print("Epoch: ",epoch)
+    for _ in range(batches_per_epoch):
+      d_model.trainable = True
 
-def train(d_model,g_model,gan_model, x_train, verbose=1, print_every=2500,ITERS = 25000):
-  latent_dim = 128
-  batch_size = 64
-  half_batch = 32
+      #print("training discriminator on real batch")
+      real_batch = grab_real_batch(x_train, half_batch)
+      d_model.train_on_batch(x=real_batch.reshape(half_batch,64,64,3), y = np.ones(half_batch))
+      #print("training discriminator on fake batch")
+      fake_batch = grab_fake_batch(g_model, half_batch)
+      d_model.train_on_batch(x=fake_batch.reshape(half_batch,64,64,3), y = np.zeros(half_batch))
 
-  for i in range(ITERS):
-    d_model.trainable = True
+      d_model.trainable = False
+      #print("training GAN on fake batch with real label to measure how well G fools D")
+      latent_points = np.random.randn(latent_dim*batch_size).reshape(batch_size,latent_dim)
+      gan_model.train_on_batch(x=latent_points, y=np.ones(batch_size))
 
-    #print("training discriminator on real batch")
-    real_batch = grab_real_batch(x_train, half_batch)
-    d_model.train_on_batch(x=real_batch.reshape(half_batch,64,64,1), y = np.ones(half_batch))
-    #print("training discriminator on fake batch")
-    fake_batch = grab_fake_batch(g_model, half_batch)
-    d_model.train_on_batch(x=fake_batch.reshape(half_batch,64,64,1), y = np.zeros(half_batch))
 
-    d_model.trainable = False
-    #print("training GAN on fake batch with real label to measure how well G fools D")
-    latent_points = np.random.randn(latent_dim*batch_size).reshape(batch_size,latent_dim)
-    gan_model.train_on_batch(x=latent_points, y=np.ones(batch_size))
 
-    if((i)%print_every==print_every-1):
-      save_plot(grab_fake_batch(g_model,50).reshape(50,64,64,3),epoch=i)
+    if(epoch%print_every==print_every-1):
+      save_plot(grab_fake_batch(g_model,50).reshape(50,64,64,3),epoch=epoch)
